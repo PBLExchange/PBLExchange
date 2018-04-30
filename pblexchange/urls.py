c6dcf8a4c7534ad7d19a0458f213c03d667eac0d
Gideon Blegmand <gidde_94@hotmail.com>
2018-04-30 11:58:32 +0200

    added about page

--- pblexchange/urls.py
+++ pblexchange/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='home'),
     url(r'^login/$', cas_views.login, name='login'),
     url(r'^logout/$', cas_views.logout, name='logout'),
+    url(r'^about/$', views.about, name='about'),
     #  News article urls TODO: use include
     url(r'^news_article/(?P<news_article_id>[0-9]+)$', views.news_article, name='news_article'),
     url(r'^news_article/write', views.write_article, name='news_article_write'),
