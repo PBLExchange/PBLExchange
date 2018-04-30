5c42f4f7008a02df18d17262cf339b5225073e7d
Gideon Blegmand <gidde_94@hotmail.com>
2018-04-30 13:54:25 +0200

    changed subscription alter-urls to use object's pk instead of name

--- pble_subscriptions/urls.py
+++ pble_subscriptions/urls.py
@@ -2,11 +2,10 @@ from django.conf.urls import url, include
 from . import views
 
 subscribe_patterns = [
-    url(r'category/(?P<category_text>[a-zA-Z0-9_\-]+)$', views.alter_categories, name='subscribe_category'),
-    url(r'tag/(?P<tag_text>[a-zA-Z0-9_\-]+)$', views.alter_tags, name='subscribe_tag'),
-    url(r'peer/(?P<username>[a-zA-Z0-9_\-]+)$', views.alter_peers, name='subscribe_peer')   # TODO: Use uid instead of username
+    url(r'category/(?P<category_id>[0-9]+)$', views.alter_categories, name='subscribe_category'),
+    url(r'tag/(?P<tag_id>[0-9]+)$', views.alter_tags, name='subscribe_tag'),
+    url(r'peer/(?P<userid>[0-9]+)$', views.alter_peers, name='subscribe_peer')
 ]
-# TODO: these urls and views should use the pk of the respective items
 
 urlpatterns = [
     url(r'^categories$', views.categories, name='categories'),
