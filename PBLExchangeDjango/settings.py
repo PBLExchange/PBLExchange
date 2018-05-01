a37f31df254ee6b92bc0c25e54aca1d49ec22fd4
Andreas H. Klostergaard <aklost11@student.aau.dk>
2018-05-01 19:45:14 +0200

    updated transfer command

--- PBLExchangeDjango/settings.py
+++ PBLExchangeDjango/settings.py
@@ -115,6 +115,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'transferfrom': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'old.sqlite')
     }
 }
 
