e3cfd6de1f5951d36c66e9a6dd388f29fe7874d0
Andreas H. Klostergaard <aklost11@student.aau.dk>
2018-05-16 13:10:09 +0200

    Added missing translations

--- pble_users/urls.py
+++ pble_users/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib.auth.models import Group, Permission
 from django.contrib.contenttypes.models import ContentType
+from django.utils.translation import ugettext_lazy as _
 from . import views
 from pble_questions.models import Question, Post
 from PBLExchangeDjango.settings import PBLE_GROUPS
@@ -10,15 +11,15 @@ detail_patterns = [
     url(r'^$', views.detail, name='detail'),
     url(r'^pble_questions$', views.question_list, {
         'questions': Question.objects.by_user,
-        'title_prefix': 'Questions by ',
+        'title_prefix': _('Questions by '),
     }, name='pble_questions'),
     url(r'^unanswered$', views.question_list, {
         'questions': Question.objects.unanswered_by_user,
-        'title_prefix': 'Unanswered questions by ',
+        'title_prefix': _('Unanswered questions by '),
     }, name='unanswered'),
     url(r'^answers$', views.question_list, {
         'questions': Question.objects.answered_by_user,
-        'title_prefix': 'Answers by ',
+        'title_prefix': _('Answers by '),
     }, name='answered'),
     url(r'^bonus_points$', views.bonus_points, name='bonus_points')
 ]
